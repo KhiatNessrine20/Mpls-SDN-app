680650cdd82108b5680036ef3056cc97bbedd45f
khiat Nessrine20 <khiatnessrine18@gmaiil.com>
2023-05-02 10:59:26 +0200

    Second commit

--- sw_stp_mpls.py
+++ sw_stp_mpls.py
@@ -6,16 +6,16 @@ from ryu.ofproto import ofproto_v1_3
 from ryu.lib import dpid as dpid_lib
 from ryu.lib import stplib
 from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
+from ryu.lib.packet import ethernet, mpls
 #from ryu.app import simple_switch_13
 from ryu.app import simple_switch_stp13
 
 
-class mplsclass (simple_switch_stp13, SimpleSwitch13):
+class mplsclass (simple_switch_stp13.SimpleSwitch13):
         OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
         
         def __init__( self, *args, **kwargs):
-            super().__init__( *args, **kwargs)
+            super(mplsclass, self).__init__( *args, **kwargs)
             self.mac_to_port= {}   # stores the mapping between the mac add and the port ( li jat meno) for each datapath
             self.dst_to_label ={}  # stores the mapping between the dest add and the label for each datapath
             self.label = 16  # initialisation des labels  
@@ -54,16 +54,83 @@ class mplsclass (simple_switch_stp13, SimpleSwitch13):
                 out_port = ofproto.OFPP_FLOOD
              # install a flow to avoid packet_in next time
             actions = [parser.OFPActionOutput(out_port)]
-            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+            if out_port != ofproto.OFPP_FLOOD:
+                match = parser.OFPMatch(in_port=in_port, eth_src=src, eth_dst=dst, eth_type=ethtype)
+                self.logger.info("Flow match: in_port=%s, src=%s, dst=%s, type=ARP", in_port, src, dst)
+                self.logger.info("Flow actions: out_port=%s", out_port)
+            # verify if we have a valid buffer_id, if yes avoid to send
+            # both # flow_mod & packet_out        
+                if msg.buffer_id != ofproto.OFP_NO_BUFFER:
                     self.add_flow(datapath, 1, match, actions, msg.buffer_id)
                     return
-            else:
+                else:
                     self.add_flow(datapath, 1, match, actions)
+            
             data = None
             if msg.buffer_id == ofproto.OFP_NO_BUFFER:
                 data = msg.data
             out = parser.OFPPacketOut(datapath=datapath,buffer_id=msg.buffer_id, in_port=in_port, actions=actions, data=data)
             datapath.send_msg(out)
+        
+        def mpls_handler(self, ev):
+            msg = ev.msg
+            datapath = msg.datapath
+            ofproto = datapath.ofproto
+            parser = datapath.ofproto_parser
+            in_port =  msg.match['in_port']
+            dpid = datapath.id
+            self.logger.info("Launching MPLS function for datatpath %s", dpid) 
+            pkt = packet.Packet(msg.data) 
+            eth = pkt.get_protocols(ethernet.ethernet)[0] 
+            dst = eth.dst
+            src = eth.src      
+            ethtype = eth.ethertype
+            mpls_proto = pkt.get_protocol(mpls.mpls)
+            out_port = self.mac_to_port[dpid][dst]
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_type=ethtype, mpls_label=mpls_proto.label)
+            self.logger.info("Flow match: in_port=%s, dst=%s, type=IP, label=%s", in_port, dst, mpls_proto.label)
+
+            if dpid == 1 : #c'est un Ler Ingress
+                self.label = self.label + 1
+                self.dst_to_label[dpid][dst] = self.label
+                actions = [parser.OFPActionPushMpls(), parser.OFPActionSetField(mpls_label=self.label), parser.OFPActionOutput(out_port)]
+                self.logger.info("Flow actions: Switch MPLS LER_In=%s, out_port=%s", self.label, out_port)
+            
+            elif dpid == 2 or dpid==3: #c'est un LSR
+                self.label = self.label + 1
+                self.dst_to_label[dpid][dst] = self.label
+                actions = [parser.OFPActionPopMpls(), parser.OFPActionPushMpls(), parser.OFPActionSetField(mpls_label=self.label), parser.OFPActionOutput(out_port)]
+                self.logger.info("Flow actions: switch MPLS LSR=%s, out_port=%s", self.label, out_port)
+            
+            elif dpid == 4:#c'est un LER Egress
+
+
+                actions = [parser.OFPActionPopMpls(), parser.OFPActionOutput(out_port)]
+                self.logger.info("Flow actions: switch MPLS LER_Eg=%s, out_port=%s", self.label, out_port)
+
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+            data = None
+            if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+                data = msg.data
+            out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port,actions=actions, data=data)
+            datapath.send_msg(out)
+            
+
+
+            
+
+
+
+            
+            
+            
+
+
+
 
         @set_ev_cls(stplib.EventPacketIn, MAIN_DISPATCHER)
         def _packet_in_handler(self, ev):
@@ -81,8 +148,10 @@ class mplsclass (simple_switch_stp13, SimpleSwitch13):
             self.mac_to_port.setdefault(dpid, {}) # creation d'un dictionnaire propore au switch ida mnsh already created with a key as dpid
             self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
 
-            if ethtype == packet.ARP_TYPE:
+            if ethtype == 2048:
                 self.arp_handler(ev)
+            elif ethtype == 2054 or ethtype == 34887:
+                self.mpls_handler(ev)
            # learn a mac address to avoid FLOOD next time.
             self.mac_to_port[dpid][src] = in_port
 
@@ -105,3 +174,4 @@ class mplsclass (simple_switch_stp13, SimpleSwitch13):
             out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
                                   in_port=in_port, actions=actions, data=data)
             datapath.send_msg(out)
+
