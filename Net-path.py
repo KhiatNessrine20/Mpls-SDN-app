7c7dbb9c4595f91adbc0dc122c9032cadd47ab4c
khiat Nessrine20 <khiatnessrine18@gmaiil.com>
2023-05-08 18:27:48 +0200

    Second commit

--- Net-path.py
+++ Net-path.py
@@ -141,3 +141,108 @@ class SimpleSwitch13(app_manager.RyuApp):
             self.switch_ports[dst_dpid]['in_port'].append(link_info['port'])
 
     # Rest of the code...
+
+
+////
+  @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+def _packet_in_handler(self, ev):
+    msg = ev.msg
+    datapath = msg.datapath
+    parser = datapath.ofproto_parser
+
+    # Get the input switch (datapath) ID
+    dpid = format(datapath.id, "d").zfill(16)
+
+    # Check if the input switch ID is in the switch_ports dictionary
+    if dpid in self.switch_ports:
+        # Get the input port for the current switch
+        in_ports = self.switch_ports[dpid]['in_port']
+
+        # Get the output ports for the current switch
+        out_ports = self.switch_ports[dpid]['out_port']
+
+        # Rest of the code for handling input/output ports
+
+        # Get the source and destination MAC addresses from the received packet
+        pkt = packet.Packet(msg.data)
+        eth_pkt = pkt.get_protocol(ethernet.ethernet)
+        src_mac = eth_pkt.src
+        dst_mac = eth_pkt.dst
+
+        # Calculate the shortest path using Networkx
+        src_switch = 's' + dpid
+        dst_switch = 's5'  # Example: Destination switch is s5
+        paths = nx.all_shortest_paths(self.net, source=src_switch, target=dst_switch)
+
+        # Assume the first shortest path as the selected path
+        selected_path = next(paths)
+
+        # Check if the destination MAC address is known
+        if dst_mac in self.mac_to_port[dpid]:
+            # Forward the packet to the output port corresponding to the destination MAC address
+            out_port = self.mac_to_port[dpid][dst_mac]
+        else:
+            # Forward the packet to all output ports
+            out_port = ofproto_v1_3.OFPP_FLOOD
+
+        # Check if the calculated output port is in the output ports list
+        if out_port in out_ports:
+            actions = [parser.OFPActionOutput(out_port)]
+
+            # Install a flow to avoid packet_in next time
+            if out_port != ofproto_v1_3.OFPP_FLOOD:
+                match = parser.OFPMatch(in_port=in_ports[0], eth_dst=dst_mac, eth_src=src_mac)
+                if msg.buffer_id != ofproto_v1_3.OFP_NO_BUFFER:
+                    self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                    return
+                else:
+                    self.add_flow(datapath, 1, match, actions)
+
+            # Send the packet out the specified output port
+            data = None
+            if msg.buffer_id == ofproto_v1_3.OFP_NO_BUFFER:
+                data = msg.data
+
+            # Forward the packet along the selected path
+            for i in range(len(selected_path) - 1):
+                current_switch = selected_path[i]
+                next_switch = selected_path[i + 1]
+
+                # Check if the current switch ID is in the switch_ports dictionary
+                if current_switch in self.switch_ports:
+                    # Get the output port from the current switch to the next switch
+                    if next_switch in self.switch_ports[current_switch]['out_port']:
+                        out_port = self.switch_ports[current_switch]['out_port'][next_switch]
+                        actions = [parser.OFPActionOutput(out_port)]
+
+                        # Send the packet out the specified output port
+                        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                                  in_port=in_ports[0], actions=actions, data=data)
+                        datap
+///////
+
+for i in range(len(path)-1):
+            src_node = path[i]
+            dst_node = path[i+1]
+            in_port = self.get_input_port(src_node)
+            out_port = self.get_output_port(dst_node)
+
+   def get_input_port(self, switch, path):
+    # Determine the input port for a switch in the path
+    if switch == path[0]:
+        return path[1]['in_port']  # Input port from the previous switch in the path
+    else:
+        # Find the link that connects to the previous switch in the path
+        for link in self.network_topology[switch]:
+            if link['dst'] == path[path.index(switch) - 1]:
+                return link['src_port']  # Input port from the link
+
+def get_output_port(self, switch, path):
+    # Determine the output port for a switch in the path
+    if switch == path[-1]:
+        return path[-2]['out_port']  # Output port to the next switch in the path
+    else:
+        # Find the link that connects to the next switch in the path
+        for link in self.network_topology[switch]:
+            if link['dst'] == path[path.index(switch) + 1]:
+                return link['dst_port']  # Output port to the link
